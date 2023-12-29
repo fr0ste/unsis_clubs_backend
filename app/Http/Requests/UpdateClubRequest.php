683112db97061f948434446868d5d73a39863f4c
fr0ste <figueroamartinezjoelfrancisco@gmail.com>
2023-12-29 12:38:17 -0600

    #15 Controller for clubs

---
+++ app/Http/Requests/UpdateClubRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateClubRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $method = $this->method();
+        if($method == 'PUT'){
+            return [
+                'name' => ['required'],
+                'description' => ['required']
+            ];
+
+        }else{
+            return [
+                'name' => ['sometimes', 'required'],
+                'description' => ['sometimes', 'required']
+            ];
+        }
+    }
+
+    protected function prepareForValidation(){
+        if($this->name){
+            $this->merge([
+                'ClubName' => $this->name
+            ]);
+        }
+
+        if($this->description){
+            $this->merge([
+                'Description' => $this->description
+            ]);
+            
+        }
+    }
+}
